faada5ba04d905b8c29ada5232ebbb7704c1fb00
James Singleton <jamesrsingleton1@gmail.com>
2020-12-20 14:14:32 -0700

    chore(accessibility): fix accessibility issues on home page

--- components/Home/Features.jsx
+++ components/Home/Features.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 const Features = () => (
-  <section className="text-gray-500 bg-gray-900 body-font">
+  <section className="text-gray-400 bg-gray-900 body-font">
     <div className="container px-5 py-24 mx-auto">
       <div className="text-center mb-20">
         <h2 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['dummyimage.com'],
   },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
 }
