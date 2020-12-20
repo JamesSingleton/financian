c2e4b32e225b90de165204804fdb0ddf36ffad35
James Singleton <jamesrsingleton1@gmail.com>
2020-12-20 13:27:35 -0700

    feat(features): update features and move them up

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,21 @@
-import Page from '@/components/Layout/Page';
-import SectionItem from '@/components/Home/SectionItem'
-import Hero from '@/components/Home/Hero'
-import Features from '@/components/Home/Features'
-import Testimonials from '@/components/Home/Testimonials'
-import SocialMeta from '@/components/SocialMeta';
 import { WEBSITE_NAME, WEBSITE_TITLE, WEBSITE_DESCRIPTION, WEBSITE_URL } from '@/lib/constants';
-import Pricing from '@/components/Pricing'
+import Page from '@/components/Layout/Page';
+import Hero from '@/components/Home/Hero'
+import SocialMeta from '@/components/SocialMeta';
 import Steps from '@/components/Home/Steps';
+import Features from '@/components/Home/Features'
 
 const Index = () => (
   <Page>
     <SocialMeta 
       image="/static/twitter-cards/hero-image.webp"
-      title={`Home | ${WEBSITE_NAME} - ${WEBSITE_TITLE}`}
+      title={`${WEBSITE_NAME} - ${WEBSITE_TITLE}`}
       url={WEBSITE_URL}
       description={WEBSITE_DESCRIPTION}
     />
     <Hero />
-    <Steps />
     <Features />
-    <Pricing />
+    <Steps />
   </Page>
 );
 
