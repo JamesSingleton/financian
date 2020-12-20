0ce684cc51425a11c88008f1f9060c920717c38a
James Singleton <jamesrsingleton1@gmail.com>
2020-12-20 13:33:14 -0700

    chore(og): add og:type of website to SocialMeta

--- components/SocialMeta.jsx
+++ components/SocialMeta.jsx
@@ -5,6 +5,7 @@ const SocialMeta = ({ title, description, image, url, keywords }) => (
   <Head>
     <meta name="twitter:site" content={`@${TWITTER_USER_NAME}`} />
     <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
+    <meta property="og:type" content="website" />
     {title && <meta property="og:title" content={title} />}
     {url && <meta property="og:url" content={url} />}
     {description && <meta name="description" content={description} />}
